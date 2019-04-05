442bb9b73d8c54ca682e1632df6fde8fb90f7b86
David Graves <dkgraves@bellsouth.net>
2019-04-04 21:15:25 -0400

    End of day changes added yelp GET

--- js/app.js
+++ js/app.js
@@ -17,6 +17,7 @@ function initMap() {
     	var name = locations[i].name;
     	var position = locations[i].location;
     	var label = locations[i].label;
+    	var yelp_id = locations[i].yelp_id
 		console.log(position)
 		console.log(name)
 		// Create a marker for each location, and put it into the markers array.
@@ -24,6 +25,7 @@ function initMap() {
 		    position: position,
 		    title: name,
 		    label: label,
+		    yelp_id: yelp_id,
 		    animation: google.maps.Animation.DROP,
 		    id: i
 		
@@ -33,14 +35,7 @@ function initMap() {
 		// Create an onclick event to open the details infowindow for each marker.
         marker.addListener('click', function() {
           populateInfoWindow(this, detailsInfowindow);
-        });
-		// Create an onclick event to open the details infowindow at each marker.
-		marker.addListener('click', function() {
-		populateInfoWindow(this, detailsInfowindow);
-		});
-		// Two event listeners - one for mouseover, one for mouseout,
-		// to change the colors back and forth.
-		
+        });	
     }
 	
     // This function populates the infowindow when the marker is clicked. We'll only allow
@@ -49,36 +44,33 @@ function initMap() {
     function populateInfoWindow(marker, infowindow) {
       // Check to make sure the infowindow is not already opened on this marker.
       if (infowindow.marker != marker) {
-        // Clear the infowindow content to give the streetview time to load.
+        // Clear the infowindow content to give yelp data time to load.
         infowindow.setContent('');
         infowindow.marker = marker;
         // Make sure the marker property is cleared if the infowindow is closed.
         infowindow.addListener('closeclick', function() {
           infowindow.marker = null;
         });
-        var streetViewService = new google.maps.StreetViewService();
-        var radius = 50;
-        // In case the status is OK, which means the pano was found, compute the
-        // position of the streetview image, then calculate the heading, then get a
-        // panorama from that and set the options
-        function getStreetView(data, status) {
-          if (status == google.maps.StreetViewStatus.OK) {
-            var nearStreetViewLocation = data.location.latLng;
-            var heading = google.maps.geometry.spherical.computeHeading(
-              nearStreetViewLocation, marker.position);
-              infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
-              var panoramaOptions = {
-                position: nearStreetViewLocation,
-                pov: {
-                  heading: heading,
-                  pitch: 30
-                }
-              };
-            var panorama = new google.maps.StreetViewPanorama(
-              document.getElementById('pano'), panoramaOptions);
+        
+        function getYelpData(marker) {
+    		$.ajax({
+    			method: "GET",
+    			headers: {"Authorization": "Bearer 4MH965vAT8BdYrnpJ9sfq7SaTeL3lwP-NURptuo5pHomWo2KvvdqX1ovRdNHpT8Ax-y0Bw9DtoyXcqoCalaXNHxLnSicA1GsTh3UQOuqXi2rlVwFOgyoV3qB_DylXHYx"},
+    			url: "https://api.yelp.com/v3/businesses/" + marker.yelp_id,
+    			dataType: "json"
+    			
+    		});
+        	
+        	
+        	if (status == "200") {
+            
+              infowindow.setContent('<div>' + marker.title + '</div><div id="yelp"></div>');
+              
+          
+            
           } else {
             infowindow.setContent('<div>' + marker.title + '</div>' +
-              '<div>No Street View Found</div>');
+              '<div>No Yelp Data Found</div>');
           }
         }
         // Use streetview service to get the closest streetview image within
