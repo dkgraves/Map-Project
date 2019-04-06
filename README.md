9ee601c510c8771545bf497653a77edd16b18c17
David Graves <dkgraves@bellsouth.net>
2019-04-06 16:04:21 -0400

    save progress-no errors, not finished

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Map-Project
+
+
+
+Google Maps API
+Using Yelp Fusion API
+https://www.bootstrapdash.com/use-bootstrap-with-html/
+https://www.w3schools.com/bootstrap/bootstrap_dropdowns.asp
--- js/app.js
+++ js/app.js
@@ -7,7 +7,9 @@ function initMap() {
     // use a constructor to create a new map JS object.
     map = new google.maps.Map(document.getElementById('map'), {
  	  center: {lat: 33.963896, lng: -84.139585},
- 	  zoom: 2
+ 	  mapTypeId: google.maps.MapTypeId.ROADMAP,
+ 	  mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
+ 	  zoom: 15
  	 });
     
     var detailsInfowindow = new google.maps.InfoWindow();
@@ -38,20 +40,25 @@ function initMap() {
         });	
     }
 	
-    // This function populates the infowindow when the marker is clicked. We'll only allow
-    // one infowindow which will open at the marker that is clicked, and populate based
-    // on that markers position.
+    // This function populates the infowindow when the marker is clicked. Only one
+    // infowindow can be open at a time. It is populate based
+    // on the markers information in the poiData.js file.
     function populateInfoWindow(marker, infowindow) {
       // Check to make sure the infowindow is not already opened on this marker.
       if (infowindow.marker != marker) {
-        // Clear the infowindow content to give yelp data time to load.
-        infowindow.setContent('');
+    	  showMarkers()
+        // Add space in the infowindow content to allow the map to shift
+    	  // else the map will not shift enough when real content arrives.
+        infowindow.setContent('<div>' + marker.title + '</div>' +
+        					'<br><br><br><br><br>' 
+        					); 
         infowindow.marker = marker;
         // Make sure the marker property is cleared if the infowindow is closed.
         infowindow.addListener('closeclick', function() {
           infowindow.marker = null;
+          showMarkers()
         });
-        
+        // This function will use the marker data to retrieve Yelp data via a GET ajax call.
         function getYelpData(marker) {
     		$.ajax({
     			method: "GET",
@@ -59,7 +66,7 @@ function initMap() {
     				"Authorization": "Bearer 4MH965vAT8BdYrnpJ9sfq7SaTeL3lwP-NURptuo5pHomWo2KvvdqX1ovRdNHpT8Ax-y0Bw9DtoyXcqoCalaXNHxLnSicA1GsTh3UQOuqXi2rlVwFOgyoV3qB_DylXHYx"},
     			url: "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/" + marker.yelp_id,
     			dataType: "json",
-    			success: function(businessDataJson) {
+    			success: function(businessDataJson) {  // Display the Yelp information
     				console.log(businessDataJson.rating);
     			 	infowindow.setContent('<div>' + marker.title + '</div>'+
     			 			'<div><img src=./img/small_' + businessDataJson.rating + '.png>&ensp;' + businessDataJson.review_count +' reviews</div>' +
@@ -67,7 +74,7 @@ function initMap() {
     			 			'<div>' + businessDataJson.location.display_address + '</div>' +
     			 			'<div>' + businessDataJson.display_phone + '</div>' +
     			 			'<a href=' + businessDataJson.url + ' target=_blank>' +
-    			 				'<img src=./img/Yelp_trademark_RGB_outline.png alt=Yelp Trademark style=height:64px>' +
+    			 				'<img src=./img/Yelp_trademark_RGB_outline.png alt=Yelp Trademark style=height:54px>' +
     			 			'</a>'	
     			 			);	
     			},
@@ -78,6 +85,7 @@ function initMap() {
     			}	
     		});
         }
+        // Call the function to get and display the Yelp information in an infowindow
         getYelpData(marker);
         // Open the infowindow on the correct marker.
         infowindow.open(map, marker);
@@ -99,7 +107,7 @@ function initMap() {
           markers[i].setMap(map);
           bounds.extend(markers[i].position);
         }
-        map.fitBounds(bounds, {top:125, bottom:50});
+        map.fitBounds(bounds);
       }
     showMarkers();
   }
